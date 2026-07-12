43d5f7366efb805e82978064e561aefce05cbb17
Rio-Sasaki <sorario10140826@gmail.com>
2026-07-12 20:39:43 +0900

    fix: レスポンシブ対応・middleware削除 #46

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -5,7 +5,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="h-full flex bg-gray-50">
       <Sidebar />
-      <main className="flex-1 overflow-y-auto pb-16 md:pb-0">{children}</main>
+      <main className="flex-1 overflow-y-auto pb-20 md:pb-0">{children}</main>
       <Navigation />
     </div>
   );
