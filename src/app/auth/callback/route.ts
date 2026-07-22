00583ac7c33db65c4f9117a081b7ca49823fa839
Rio-Sasaki <sorario10140826@gmail.com>
2026-07-22 11:32:56 +0900

    fix:idを削除

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -25,7 +25,7 @@ export async function GET(request: NextRequest) {
       }
     );
 
-    const { data, error } = await supabase.auth.exchangeCodeForSession(code);
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
       return NextResponse.redirect(`${origin}/dashboard`);
     }
